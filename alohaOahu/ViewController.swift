a5df1690aba901edbfe0e5b85bffca246bd9c161
Tam Phan <tam.phan@mac.com>
2018-10-30 21:39:47 -0700

    first commit

---
+++ alohaOahu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alohaOahu
+//
+//  Created by Tam Phan on 10/30/18.
+//  Copyright © 2018 Tam Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
